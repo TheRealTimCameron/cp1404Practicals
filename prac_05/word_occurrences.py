a0d829bee085bd02c7b441735c41a311f602679e
jc493722 <timothy.cameron1@my.jcu.edu.au>
2019-04-11 13:12:39 +1000

    Finished all prac, finishing pull requests part

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,4 +2,11 @@ words_counted = {}
 text_string = str(input("Text: "))
 test = text_string.split(" ")
 for word in test:
-    print("{} : {}".format(word, len(word)))
+    frequency = words_counted.get(word, 0)
+    words_counted[word] = frequency + 1
+test = list(words_counted.keys())
+test.sort()
+
+length_calculator = max(len(i) for i in test)
+for i in test:
+    print("{:{}} : {}".format(i, length_calculator, words_counted[i]))
